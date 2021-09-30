955fef69801edb143209525dddfaa00820508dcd
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-09-30 12:25:46 +1000

    #5 Links in NextJS

--- components/nav.js
+++ components/nav.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
+  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+].map((link) => {
   link.key = `nav-link-${link.href}-${link.label}`;
   return link;
 });
