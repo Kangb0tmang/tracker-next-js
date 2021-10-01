2677316de2770536d4b48441d10f54b4c38a0b5e
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-10-01 11:58:23 +1000

    #7 Setting up Apollo Server, fix for newer graphql version

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,10 +1,65 @@
-// Short way
-export default (req, res) => {
-  // NextJS helpers
-  res.status(200).json({
-    test: 'Hello there',
-  });
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = gql`
+  type User {
+    id: ID
+  }
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return 'Hello there';
+    },
+  },
 };
+
+// https://lyonwj.com/blog/graphql-server-next-js-neo4j-aura-vercel#using-graphql-playground-with-apollo-server-v3
+// GraphQL Playground deprecated, can enable it again as a plugin
+// Should be a public GraphQL API
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+/* Example from video, was for older version */
+// Config is the same
+
+// const apolloServer = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+// const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+/* Previous basic API example */
+// Short way
+// export default (req, res) => {
+//   // NextJS helpers
+//   res.status(200).json({
+//     test: 'Hello there',
+//   });
+// };
 
 // Long way
 // export default (req, res) => {
